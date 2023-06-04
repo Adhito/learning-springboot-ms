c1e83ade68d8d4cd50ecabf40ffbb5372c2dec54
adhito909 <adhito909@gmail.com>
2023-06-04 11:40:09 +0700

    Update limit-service to get configuration from application.properties

--- limits-service/src/main/java/com/learningspringbootms/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/learningspringbootms/limitsservice/controller/LimitsController.java
@@ -1,15 +1,21 @@
 package com.learningspringbootms.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learningspringbootms.limitsservice.bean.Limits;
+import com.learningspringbootms.limitsservice.configuration.Configuration;
 
 @RestController
 public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
 
 	@GetMapping("/limits")
 	public Limits retreiveLimits() {
-		return new Limits(1,1000);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
+		// return new Limits(1,1000);
 	}
 }
